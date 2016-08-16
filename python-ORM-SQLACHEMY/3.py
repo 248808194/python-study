c441f3f8dddb5a92bda7427a20b6841dfb60a0a6
zhoutao@zhoutao.name <123.com>
2016-08-16 16:23:12 +0800

    asdf

--- python-ORM-SQLACHEMY/3.py
+++ python-ORM-SQLACHEMY/3.py
@@ -15,39 +15,61 @@ engine = create_engine("mysql+pymysql://zhoutao:123@192.168.77.129:3306/S13",max
 Base = declarative_base()
 
 
-class Server(Base):
-    __tablename__ = 'server'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    hostname = Column(String(64), unique=True, nullable=False)
-    __table_args__ = {
-        'mysql_engine': 'InnoDB',
-        'mysql_charset': 'utf8'
-    }
-
+#一对多
 
 class Group(Base):
-    __tablename__ = 'group'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(64), unique=True, nullable=False)
-    port = Column(Integer, default=22)
-    __table_args__ = {
-        'mysql_engine': 'InnoDB',
-        'mysql_charset': 'utf8'
+    __tablename__ = "group"
+    nid = Column(Integer,primary_key=True,autoincrement=True)
+    caption = Column(String(32))
+    __table_args__={
+        "mysql_engine":"innodb",
+        "mysql_charset":"utf8"
     }
 
 
-class ServerToGroup(Base):
-    __tablename__ = 'servertogroup'
-    nid = Column(Integer, primary_key=True, autoincrement=True)
-    server_id = Column(Integer, ForeignKey('server.id'))
-    group_id = Column(Integer, ForeignKey('group.id'))
-    __table_args__ = {
-        'mysql_engine': 'InnoDB',
-        'mysql_charset': 'utf8'
+class User(Base):
+    __tablename__="user"
+    nid = Column(Integer,primary_key=True,autoincrement=True)
+    username=Column(String(32))
+    groupid=Column(ForeignKey("group.nid"))
+    #仅用于查询方便
+    group=relationship("Group",backref="uuu") #uuu指的是组里面所有用户
+    __table_args__={
+        "mysql_engine":"innodb",
+        "mysql_charset":"utf8"
     }
+    def __repr__(self): #利用类repr方法，返回 可阅读类型
+        temp = "%s|%s|%s"%(self.nid,self.username,self.groupid)
+        return  temp
 
-#执行创建表操作
+
+
 def init_db():
     Base.metadata.create_all(engine)
 init_db()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# session.query(User).delete()
+# session.query(Group).delete()
+# session.commit()
+# session.add_all([
+#     Group(nid=3,caption="SA"),
+#     User(username="zhoutao",groupid=3)
+# ])
+
+session.commit()
+#
+# ret =session.query(User).join(Group) #生成的sql语句
+# print(ret)
+# result = session.query(User.username,Group.caption).join(Group,isouter=True).all() #一对多连表查询 ,查询user表中的username 和group表里的caption
+# print(result)
+# #select * from user left join group on user.group_id = group.nid
+
+#反向查找
+
+obj = session.query(Group).filter(Group.caption == 'SA').first()
+print(obj.nid)
+print(obj.caption)
+print(obj.uuu)
